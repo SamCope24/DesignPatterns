9a42a291fde8671f4c8ef16294015420758ceab4
Sam <samcope24@gmail.com>
2023-09-08 09:17:53 +0100

    added an example of how to use yield break

--- Iterator/Iterator.cs
+++ Iterator/Iterator.cs
@@ -1,11 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-
 namespace DesignPatterns.Iterator;
 
-public class Iterator
+public static class Iterator
 {
-    public IEnumerable<int> ReturnEvenNumbers(int endingNumber)
+    public static IEnumerable<int> ReturnEvenNumbers(int endingNumber)
     {
         var evens = new List<int>();
 
@@ -22,13 +19,19 @@ public class Iterator
 
     private static bool IsEven(int number) => number % 2 == 0;
 
-    public IEnumerable<int> YieldReturnEvenNumbers(int endingNumber)
+    public static IEnumerable<int> YieldReturnEvenNumbers(int endingNumber)
     {
         for (int i = 1; i <= endingNumber; i++)
         {
             if (IsEven(i))
             {
                 yield return i;
+
+                if (i == 10000)
+                {
+                    Console.WriteLine("Nope, you've had enough evens for today sunshine!");
+                    yield break;
+                }
             }
         }
     }
